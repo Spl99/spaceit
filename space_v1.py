0871f5975abc285f180b8b8cc410d4ed541965b5
Sam <samplong@outlook.com>
2023-06-27 14:20:27 +0100

    Cleaned up and added contract + agent menus and views

--- space_v1.py
+++ space_v1.py
@@ -5,42 +5,80 @@ API_TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiRUNITzIxOS
 
 def agent_details():
 
-     url = f'{API_BASE_URL}/my/agent'
-     headers = {
-            'Authorization': f'Bearer {API_TOKEN}'
-     }
+    url = f'{API_BASE_URL}/my/agent'
+    headers = {
+        'Authorization': f'Bearer {API_TOKEN}'
+    }
 
-     response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    symbol = data['data']['symbol']
+    headquarters = data['data']['headquarters']
+    credits = data['data']['credits']
 
-     print(response.status_code)
-     print(response.json())
+    print(f"Symbol: {symbol}")
+    print(f"Headquarters: {headquarters}")
+    print(f"Credits: {credits}")
+
+
+    #print(response.status_code)
+    #print(response.json())
     
 
 def contract_details():
 
-     url = f'{API_BASE_URL}/my/contracts'
-     headers = {
-            'Authorization': f'Bearer {API_TOKEN}'
-                }
-     response = requests.get(url, headers=headers)
-     print (response.json())
+    url = f'{API_BASE_URL}/my/contracts'
+    headers = {
+        'Authorization': f'Bearer {API_TOKEN}'
+    }
+       
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    for contract in data['data']:
+        contract_type = contract['type']
+        deadline = contract['terms']['deadline']
+        payment_on_fulfilled = contract['terms']['payment']['onFulfilled']
+        deliveries = contract['terms']['deliver']
+
+        print(f"Contract Type: {contract_type}")
+        print(f"Contract Deadline: {deadline}")
+        print(f"Payment: {payment_on_fulfilled}")
+        print("Delivery Requirements: ")
+        print("=======================")
+
+        for delivery in deliveries:
+            trade_symbol = delivery['tradeSymbol']
+            destination = delivery['destinationSymbol']
+            units_required = delivery['unitsRequired']
+            units_fulfilled = delivery['unitsFulfilled']
+
+
+            print(f"Trade Symbol: {trade_symbol}")
+            print(f"Destination: {destination}")
+            print(f"Units Required: {units_required}")
+            print(f"Units Fulfilled: {units_fulfilled}")
+
+    #print (response.json())
 
 
 def menu():
     global choice
+    print()
     print("Menu")
     print("1. Agent Details")
-    print ("2. Contact Details")
-
-    choice = input("Enter an option")
+    print("2. Contact Details")
+    print("0. To exit")
+    print()
+    choice = input("Enter an option: ")
 
     if choice == "1":
         agent_details()
+        print()
     elif choice == "2":
         contract_details()
     elif choice == "0":
-        print("Exiting....")
-
+        print("Exiting...")
+        
     else:
         print("Invalid, try again")
 
